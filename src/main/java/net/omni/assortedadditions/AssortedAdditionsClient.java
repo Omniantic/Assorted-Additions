920da90a9557bd798fd2622682cfbcaaa7753746
Omniantic <omniantic@gmail.com>
2024-12-04 13:10:52 +0000

    Initial commit

---
+++ src/main/java/net/omni/assortedadditions/AssortedAdditionsClient.java
@@ -0,0 +1,10 @@
+package net.omni.assortedadditions;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AssortedAdditionsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
